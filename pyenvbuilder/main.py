80e0cc17bbe411fc66a948d752c80ee579df28f7
Cristina Sewell <csewell@slac.stanford.edu>
2020-04-06 18:12:10 -0700

    BLD: added __main__.py file and fixed some pep8 issues

--- pyenvbuilder/main.py
+++ pyenvbuilder/main.py
@@ -1,11 +1,51 @@
 '''
-penvbuilder entry point
+pyenvbuilder's commands
 '''
-from pyenvbuilder.commands import add_commands
+import argparse
+import logging
+from pyenvbuilder import __version__
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def launch(log_level="INFO"):
+    '''
+    PyEnvBuilder's Launcher
+    '''
+    pyenvbuilder_logger = logging.getLogger('')
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '[%(asctime)s] [%(levelname)s] - %(message)s')
+    handler.setFormatter(formatter)
+    pyenvbuilder_logger.addHandler(handler)
+    pyenvbuilder_logger.setLevel(log_level)
+    handler.setLevel(log_level)
+
+
+def parse_arguments(*args, **kwargs):
+    '''
+    Defines shell commands
+    '''
+
+    project_desc = "Python Environment Builder"
+    parser = argparse.ArgumentParser(description=project_desc)
+
+    parser.add_argument(
+        '--version', action='version',
+        version='PyEnvBuilder {version}'.format(version=__version__),
+        help="show pyenvbuilder's version number and exit")
+
+    parser.add_argument(
+        '--log_level',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        default='INFO',
+        help='configure log level')
+
+    return parser.parse_args(*args, **kwargs)
+
 
 def main():
-    add_commands()
-
-
-if "__name__" == "__main":
-    main()
+    args = parse_arguments()
+    kwargs = vars(args)
+    launch(**kwargs)
