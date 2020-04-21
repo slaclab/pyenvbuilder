31f16e4d6089b59f474617d351b3e63ab140aa0a
crinna23 <cris.midrigan@gmail.com>
2020-04-21 09:55:05 -0700

    WIP - changed to bash_script - source activation not functioning yet

--- pyenvbuilder/commands/create.py
+++ pyenvbuilder/commands/create.py
@@ -2,7 +2,10 @@
 Class for the create command
 '''
 import logging
+from string import Template
 import subprocess
+import stat
+from pathlib import Path
 
 from .interface import Command
 from .check import Check
@@ -18,79 +21,80 @@ class Create(Command):
         self.name = 'create'
         self.help = 'Creates an environment, given an YAML file'
 
-    def run(self, **kwargs): 
+    def run(self, **kwargs):
         # invoke conda to see if installed
         if validate_installed('conda')[0]:
             # validate files' location
             files = locate_files(**kwargs)
             for f in files:
-            # check if the files are valid YAML files
+                # check if the files are valid YAML files
                 if check.yaml_validator(f)[0]:
-                     data = check.yaml_loader(f)
-                     if data:
-                         self.conda_create(data)
-
+                    data = check.yaml_loader(f)
+                    if data:
+                        self.conda_create(data)
 
     def add_args(self, cmd_parser):
-       cmd_parser.add_argument(
+        cmd_parser.add_argument(
             '--skip-tests', action='store_true', default=False)
-       cmd_parser.add_argument(
+        cmd_parser.add_argument(
             'files', nargs='+')
 
-    def conda_create(self, data):
+    def create_script(self, data):
         env_name = data['name']
         env_version = data['version']
         versioned_name = f'{env_name}_{env_version}'
         conda_packages = ' '.join(data['conda_packages'])
-        pip_packages = ' '.join(data['pip_packages'])
+        pip_packages = ' '.join(data['pip_packages'] or [])
 
-        conda_args = [f'conda create -n {versioned_name} -c conda-forge {conda_packages}']
-        pip_args = [f'pip install {pip_packages}']
-       # activate_args = ['eval "$$(conda shell.bash hook)" || :\n'
-               # 'conda activate $versioned_name\n']
-       # conda_avtiv = '$CONDA_PREFIX/etc/profile.d/conda.sh && conda activate {versioned_name}'
-        activate_args = [f'conda activate {versioned_name}']
-        deactivate_args = [f'conda deactivate']
+        header_template = (
+            '#!/bin/bash\n')
 
+        conda_create_template = Template(
+            '$header\n'
+            'conda create -n $versioned_name -c conda-forge $conda_packages\n'
+            'source activate $versioned_name\n')
+
+        pip_template = Template(
+            'pip install $pip_packages\n')
+
+        file_name = 'create_script'
+        try:
+            with open(file_name, 'w') as f:
+                f.write(conda_create_template.substitute(
+                    conda_packages=conda_packages,
+                    versioned_name=versioned_name, header=header_template))
+                if pip_packages.rstrip():
+                    f.write(pip_template.substitute(
+                        pip_packages=pip_packages))
+                f.flush()
+            return file_name
+        except OSError as err:
+            logger.error(f'Error when writing to {file_name}: {err}')
+        except TypeError as err:
+            logger.error(f'Error when writing {file_name}: {err}')
+        except SyntaxError as err:
+            logger.error(f'Syntax Error: {err}')
+
+    def conda_create(self, data):
         '''
         here you have to check if pip-packages are not null
-        ... and if tests are not null!!!!!
+        ... and if tests are not null - since they are not mandatory!!!!!
         '''
+        file_script = self.create_script(data)
+        f = Path(file_script)
+        f.chmod(f.stat().st_mode | stat.S_IEXEC)
+        proc_args = Path(file_script).absolute()
+        print(proc_args)
+
         try:
-            # create env with conda_packages
-            if True: #self.run_subprocess(conda_args).returncode == 0:
-                logger.info('Successfully installed conda-forge packages')
-                # activate environment - conda init???
-                if subprocess.run('eval "$(conda shell.bash hook)"', shell=True).returncode == 0:
-                    print(f'something ----------------')
-                    logger.info(f'Activated {versioned_name} environment')
-                    if subprocess.run(f'conda activate {versioned_name}', shell=True).returncode == 0:
-                        print('go it--------')
-                    # install pip packages 
-                   # if self.run_subprocess(pip_args).returncode == 0:
-                    #    logger.info('Successfully installed pip-packages')
-                     #   if self.run_subprocess(deactivate_args).returncode == 0:
-                      #      logger.info(f'{versioned_naem} deactivated')
-            else:
-                logger.error("conda create Errors...")
-
-                
-            # activate the environment just created
-            # install packages using pip
-            
-            # deactivate environment
-
-            ''' conda create -n $name -c conda-forge $ packages'''
-            ''' conda activate $name'''
-            ''' pip install $ packages '''
-            # create and environment with conda-forge
-            # activate it
-            # install pip stuff
-
-        except (subprocess.CalledProcessError, FileNotFoundError, OSError):
-
-            print('some error conda-create')
-
-    def run_subprocess(self, proc_args):
-        return subprocess.run(proc_args, check=True, shell=True)
-
+            subprocess.run(f'{proc_args}')
+            # this next guy runs fine but activates the base env instead
+            # left it here to try more with it
+            # procc = subprocess.run(
+            #    'source activate dummy_name_v0.0.0', executable='/bin/bash')
+        except subprocess.CalledProcessError as err:
+            logger.error(f'Some error here...calledProcessError {err}')
+        except FileNotFoundError as err:
+            logger.error(f'Some error here...FileNotFoundError: {err}')
+        except OSError as err:
+            logger.error(f'Some errors are occuring here.....{err}')
