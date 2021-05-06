089b9059d699e804031ca8f9e5f39c2b6f754c36
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-05-06 21:54:10 +0000

    Update docs after building Travis build 9 of slaclab/pyenvbuilder
    The docs were built from the branch 'master' against the commit
    610674473c7d0f3b1733231f624ba9a9f7a9f908.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/slaclab/pyenvbuilder/jobs/503541251.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0+52.g971a09a',
+    VERSION: '0.0.0+57.g6106744',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
